a84897c17ad192d4a13d9f60ce0e182213dba4da
Md Safayet Ullah <51539424+safayetu@users.noreply.github.com>
2019-07-23 20:46:17 +0200

    Add files via upload

---
+++ Gitsum.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1 = 5, Number2 = 7, sum;
+
+            sum = Number1 + Number2;
+
+            Console.WriteLine("The result is: " + sum);
+        }
+    }
+}
